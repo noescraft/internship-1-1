f444e0e82bd72a8abf7850b139350781d54f6eb7
noescraft <noescraft@gmail.com>
2023-06-21 19:21:21 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+    return (
+        <div className="container">
+            <h1>Sharetrade</h1>
+        </div>
+    );
+}
+
+export default App;
